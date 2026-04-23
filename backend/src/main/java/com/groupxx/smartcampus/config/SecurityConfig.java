526e9438fe930065a4db2c96733e78ae5cd9c39e
Dilshan Kohilamulla <kadilshan5@gmail.com>
2026-04-23 21:53:28 +0530

    fix: error fix

--- backend/src/main/java/com/groupxx/smartcampus/config/SecurityConfig.java
+++ backend/src/main/java/com/groupxx/smartcampus/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
